1f796b599bbc96581b78481e21a3323a07dfbfa7
berkangormus <berkangormus@outlook.com>
2025-11-19 23:15:08 +0300

    Start

---
+++ src/api/axiosInstance.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+const axiosInstance = axios.create({
+  baseURL: 'https://66b1f8e71ca8ad33d4f5f63e.mockapi.io',
+  timeout: 10000,
+  headers: { 'Content-Type': 'application/json' },
+})
+
+export default axiosInstance
